c583999e818abace8f9ccab2ae092d7ec28f01cb
dsatyap <divyakonaka08@gmail.com>
2021-01-03 11:21:03 -0500

    Update ddb.py

--- ddb.py
+++ ddb.py
@@ -7,7 +7,7 @@ def main():
     bucket = s3.Bucket("careercompassri-test-deploy")
     print (bucket)
     bucket.get_available_subresources()
-    files = bucket.objects.filter(Prefix="AWSDynamoDB/01609430713846-ba34aa1b/data/*")
+    files = bucket.objects.filter(Prefix="AWSDynamoDB/01609430713846-ba34aa1b/data/")
     for f in files:
         print (f)
 main()
