7901786993ee6eb65badc80e1d19d903ecff3ef3
dsatyap <divyakonaka08@gmail.com>
2021-01-03 11:29:09 -0500

    Update ddb.py

--- ddb.py
+++ ddb.py
@@ -1,13 +1,21 @@
 import boto3
 import json
 import os
+import gzip
 
 def main():
     s3 = boto3.resource("s3")
     bucket = s3.Bucket("careercompassri-test-deploy")
     print (bucket)
-    bucket.get_available_subresources()
+    
+    ddb = boto3.resource('dynamodb')
+    tablename = os.environ['TABLE_NAME']
+    table = ddb.Table(tablename)
+    
     files = bucket.objects.filter(Prefix="AWSDynamoDB/01609430713846-ba34aa1b/data/")
     for f in files:
         print (f)
+        with gzip.open(f, 'r') as fin:
+            data = json.loads(fin.read().decode('utf-8'))
+    print (data)
 main()
