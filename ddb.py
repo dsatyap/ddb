49ccbcb8d3496fd9a518ac8447b6259f09ffce16
dsatyap <divyakonaka08@gmail.com>
2021-01-03 11:16:50 -0500

    Update ddb.py

--- ddb.py
+++ ddb.py
@@ -8,5 +8,6 @@ def main():
     print (bucket)
     bucket.get_available_subresources()
     files = bucket.objects.filter(Prefix="AWSDynamoDB/01609430713846-ba34aa1b/data/*")
-    print (files)
+    for f in files:
+        print (f)
 main()
