49c1bc6b015baade34bb0994819259e570d8d1fc
dsatyap <divyakonaka08@gmail.com>
2021-01-03 11:55:35 -0500

    Update ddb.py

--- ddb.py
+++ ddb.py
@@ -16,6 +16,8 @@ def main():
     for obj in files:
         print('{0}'.format(obj.key))
         print (obj.key.split('/'))[-1]
+        f = obj.key.split('/')[-1]
+        print (f)
     
     with gzip.open("AWSDynamoDB/01609430713846-ba34aa1b/data/5lc3kqepyu6sjo64cbdpb2xfmu.json.gz", "r") as fin:
         data = fin.read()
