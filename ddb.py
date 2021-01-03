5622a76f6e6b2ce1608d13d147cfd9b03cd13ddd
dsatyap <divyakonaka08@gmail.com>
2021-01-03 10:16:52 -0500

    Update ddb.py

--- ddb.py
+++ ddb.py
@@ -2,8 +2,10 @@ import boto3
 import json
 import os
 
-def import_into_ddb():
+def main():
     
     s3 = boto3.resource("s3")
     bucket = s3.Bucket("careercompassri-test-deploy")
     print (bucket)
+
+main()
