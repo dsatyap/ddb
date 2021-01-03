4b941e33ce1bfaaefe985bafef5bed94cce919de
dsatyap <divyakonaka08@gmail.com>
2021-01-03 10:39:12 -0500

    Update ddb.py

--- ddb.py
+++ ddb.py
@@ -3,10 +3,11 @@ import json
 import os
 
 def main():
-    
+    client = boto3.client('s3')
     s3 = boto3.resource("s3")
     bucket = s3.Bucket("careercompassri-test-deploy")
     print (bucket)
-    print (bucket.get_available_subresources())
-
+    bucket.get_available_subresources()
+    response = client.get_bucket_policy(Bucket='careercompassri-test-deploy')
+    print (response)
 main()
