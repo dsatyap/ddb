4adf3a283e6a62a49427af4734d49137082f8a32
dsatyap <divyakonaka08@gmail.com>
2021-01-04 13:17:14 -0500

    Update ddb.py

--- ddb.py
+++ ddb.py
@@ -13,24 +13,15 @@ def main():
     bucket = s3.Bucket("careercompassri-test-deploy")
     print(bucket)
 
-    # ddb = boto3.resource('dynamodb')
     ddb_client = boto3.client('dynamodb')
-    #tablename = os.environ['TABLE_NAME']
-    # table = ddb.Table('test-shivam-dynamodb')
-
+    
     files = bucket.objects.filter(
         Prefix="AWSDynamoDB/01609430713846-ba34aa1b/data/")
     for obj in files:
         print('{0}'.format(obj.key))
         print('{0} is obj'.format(obj))
         obj = bucket.Object(obj.key)
-        obj_list = (obj.key.split('/'))
-        obj_str = str(obj_list[-1])
-        print(obj_str)
-        data = io.BytesIO()
-        # obj.download_fileobj(data)
-        # data.seek(0)
-        print(data.getvalue())
+        
         
         with gzip.GzipFile(fileobj=obj.get()["Body"]) as gzipfile:
             content = gzipfile.readlines()
