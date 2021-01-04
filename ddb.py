8349061679247f9f7963e34256286991e2e1ad6f
dsatyap <divyakonaka08@gmail.com>
2021-01-04 11:51:26 -0500

    Update ddb.py

--- ddb.py
+++ ddb.py
@@ -1,31 +1,40 @@
+
 import boto3
 import json
 import os
 import gzip
 import io
 
+
 def main():
     s3 = boto3.resource("s3")
     bucket = s3.Bucket("careercompassri-test-deploy")
-    print (bucket)
-    
+    print(bucket)
+
     ddb = boto3.resource('dynamodb')
     #tablename = os.environ['TABLE_NAME']
     #table = ddb.Table(tablename)
-    
+
     files = bucket.objects.filter(Prefix="AWSDynamoDB/01609430713846-ba34aa1b/data/")
     for obj in files:
         print('{0}'.format(obj.key))
+        print('{0} is obj'.format(obj))
         obj = bucket.Object(obj.key)
-        print ('{0} is obj'.format(obj))
         obj_list = (obj.key.split('/'))
         obj_str = str(obj_list[-1])
-        print (obj_str)
+        print(obj_str)
         data = io.BytesIO()
-        obj.download_fileobj(data)
-        with gzip.open(data.getvalue(), "r") as fin:
-            content = fin.read()
-            j = json.loads (content.decode('utf-8'))
-            print(type(j)) 
+        # obj.download_fileobj(data)
+        # data.seek(0)
+        print(data.getvalue())
+        # with gzip.open(data.getvalue(), "rb") as fin:
+        #     content = fin.read()
+        #     print(content)
+        #     j = json.loads(content.decode('utf-8'))
+        #     print(type(j))
+        with gzip.GzipFile(fileobj=obj.get()["Body"]) as gzipfile:
+            content = gzipfile.read()
+        print(content)
+
 
 main()
