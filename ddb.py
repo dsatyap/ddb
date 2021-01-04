bdaacfd82f956012aa26f14296fc5feb43a8cc9e
dsatyap <divyakonaka08@gmail.com>
2021-01-04 10:34:57 -0500

    Update ddb.py

--- ddb.py
+++ ddb.py
@@ -21,11 +21,15 @@ def main():
         obj_list = (obj.key.split('/'))
         obj_str = str(obj_list[-1])
         print (obj_str)
-
-        with open (obj_str, 'wb') as objdata:
-            obj.download_fileobj(objdata)
-            new_dict = json.loads(objdata.getvalue().decode("utf-8"))
-            print (type(new_dict))
+        data = io.BytesIO()
+        obj.download_fileobj(data)
+        new_dict = json.loads(data.getvalue().decode("utf-8"))
+        print(new_dict['status']) 
+        
+        # with open (obj_str, 'wb') as objdata:
+        #     obj.download_fileobj(objdata)
+        #     new_dict = json.loads(objdata.getvalue().decode("utf-8"))
+        #     print (type(new_dict))
         #with gzip.open(contents.getvalue(), "r") as fin:
     
         #with gzip.open(obj_str, "r") as fin:
